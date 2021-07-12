3cb92f583a762bf136c5db5a9be0d824ce47fafb
galinavrn <78894280+galinavrn@users.noreply.github.com>
2021-07-12 17:42:14 +0900

    uploading

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 /*----------------------------------------------------------------------------------------------------------------------------*/
 /*-----------------------------------------------------------------------------------------------------------------burger-menu*/
-const burger = document.querySelector('.header_burger-wrap');
+const burger = document.querySelector('.burger__wrapper');
 
 if (burger) {
-  const header = document.querySelector('.header');
-  const menu = document.querySelector('.header_menu-wrap');
+  const header = document.querySelector('.header__wrapper');
+  const navigation = document.querySelector('.navigation__wrapper');
   burger.addEventListener('click', function() {
-    document.body.classList.toggle('active');
-    header.classList.toggle('active');
     burger.classList.toggle('active');
-    menu.classList.toggle('active');
+    header.classList.toggle('active');
+    navigation.classList.toggle('active');
+    document.body.classList.toggle('active');
   });
 }
+/*----------------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------header*/
+const headerElement = document.querySelector('.header');
+
+const scrollClass = function (entries, observer) {
+  if (entries[0].isIntersecting) {
+    headerElement.classList.remove('scroll');
+  } else {
+    headerElement.classList.add('scroll');
+  }
+};
+
+const headerObserver = new IntersectionObserver(scrollClass);
+headerObserver.observe(headerElement);
 /*----------------------------------------------------------------------------------------------------------------------------*/
 /*-------------------------------------------------------------------------------------------------------------------------end*/
