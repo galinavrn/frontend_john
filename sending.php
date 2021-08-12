9572b1ec5871dd361f1819253c4e601099837f8b
galinavrn <78894280+galinavrn@users.noreply.github.com>
2021-08-12 19:31:14 +0900

    changing

--- sending.php
+++ sending.php
@@ -1,47 +1,60 @@
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exeption;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
 
-  require "phpmailer/src/Exeption.php";
   require "phpmailer/src/PHPMailer.php";
-
+  require "phpmailer/src/Exception.php";
+  require "phpmailer/src/SMTP.php";
+  
   $mail = new PHPMailer(true);
   $mail->CharSet = "UTF-8";
   $mail->setLanguage("en", "phpmailer/language/");
-  $mail->IsHTML(true);
+
+  #$mail->isSMTP();                                            //Send using SMTP
+  #$mail->SMTPDebug = 0;                                       //Enable verbose debug output
+  #$mail->Host       = "smtp.gmail.com";                       //Set the SMTP server to send through
+  #$mail->Port       = 587;
+  #$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+  #$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+  #$mail->Username   = "name.surname@gmail.com";               //SMTP username
+  #$mail->Password   = "hello";                                //SMTP password
+  #$mail->SMTPSecure = "tls";                                  //Enable implicit TLS encryption
+    
 
   #from who
-  $mail->setForm("galina.shadow@gmail.ru", "Who");
+  $mail->setFrom("name.surname@gmail.com", "Name Surname");
   #for who
-  $mail->addAddress("galina.shadow@gmail.ru");
+  $mail->addAddress("name.surname@gmail.com", "Name Surname");
   #theme of letter
   $mail->Subject = "New message from www.johndoe.com";
+  $mail->IsHTML(true);
 
   $radio = "Cookies";
-  if ($_post["form_radio"] == "icecream") {
-    $form_radio = "Icecream";
+  if ($_POST["radio"] == "icecream") {
+    $radio = "Icecream";
   }
 
   $body = "<h1>New letter!</h1>";
 
-  if (trim(!empty($_post["form_name"]))) {
-    $body.= "<p><strong>Name:</strong> ".$_post["form_name"]."</p>";
+  if (trim(!empty($_POST["name"]))) {
+    $body.= "<p><strong>Name:</strong> ".$_POST["name"]."</p>";
   }
-  if (trim(!empty($_post["form_email"]))) {
-    $body.= "<p><strong>E-mail:</strong> ".$_post["form_email"]."</p>";
+  if (trim(!empty($_POST["email"]))) {
+    $body.= "<p><strong>E-mail:</strong> ".$_POST["email"]."</p>";
   }
-  if (trim(!empty($_post["form_select"]))) {
-    $body.= "<p><strong>Theme:</strong> ".$_post["form_select"]."</p>";
+  if (trim(!empty($_POST["dropdown"]))) {
+    $body.= "<p><strong>Theme:</strong> ".$_POST["dropdown"]."</p>";
   }
-  if (trim(!empty($_post["form_message"]))) {
-    $body.= "<p><strong>Message:</strong> ".$_post["form_message"]."</p>";
+  if (trim(!empty($_POST["text"]))) {
+    $body.= "<p><strong>Message:</strong> ".$_POST["text"]."</p>";
   }
-  if (trim(!empty($_post["form_radio"]))) {
+  if (trim(!empty($_POST["radio"]))) {
     $body.= "<p><strong>Choise:</strong> ".$radio."</p>";
   }
-  if (trim(!empty($_files["form_file"]["tmp_name"]))) {
-    $filePath = __dir__ . "/file/" . $_files["form_file"]["name"];
-    if (copy($_files["form_file"]["tmp_name"], $filePath)) {
+  if (trim(!empty($_FILES["file"]["tmp_name"]))) {
+    $filePath = __dir__ . "/file/" . $_FILES["file"]["name"];
+    if (copy($_FILES["file"]["tmp_name"], $filePath)) {
       $body.= "<p><strong>Atached file below:</strong></p>";
       $mail->addAttachment($filePath);
     }
@@ -51,12 +64,12 @@
 
   #sending
   if (!$mail->send()) {
-    $result = "Error";
+    $message = "Error";
   } else {
-    $result = "Data tranfered!";
+    $message = "Data transferred!";
   }
 
-  $response = ["message" => $result];
+  $response = ["message" => $message];
 
   header("Content-type: application/json");
   echo json_encode($response);
